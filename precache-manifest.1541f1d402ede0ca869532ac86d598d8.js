46f5858697621b8073d27e1a6c3ab5d42e01a8f6
Ayoub <ayoub@aabass.net>
2019-10-31 21:23:36 +0100

    Updates

---
+++ precache-manifest.1541f1d402ede0ca869532ac86d598d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db50857bc21aa63bdeaf8654e300a26",
+    "url": "/simple-icons-website/index.html"
+  },
+  {
+    "revision": "555e795a256745caf250",
+    "url": "/simple-icons-website/static/js/2.88c5c263.chunk.js"
+  },
+  {
+    "revision": "7ab9b8c0e190b2399818",
+    "url": "/simple-icons-website/static/js/main.7b590d81.chunk.js"
+  },
+  {
+    "revision": "00297b8078df7ffb97c1",
+    "url": "/simple-icons-website/static/js/runtime-main.21b3c2d7.js"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/simple-icons-website/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "06851a1932d65340db2c1ba0faf7d6ce",
+    "url": "/simple-icons-website/static/media/npm.06851a19.svg"
+  },
+  {
+    "revision": "3f588347e9c1400ba42ce310f2da4876",
+    "url": "/simple-icons-website/static/media/simpleicons.3f588347.svg"
+  }
+]);
